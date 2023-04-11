b482609f341df93b6e19274a2e82a68a20395f4a
MAUMOREIRA <mauriciomoreirajr2006@gmail.com>
2023-04-11 12:10:35 -0300

    ACABEIIII

--- Prova 1º Bimestre/3110.php
+++ Prova 1º Bimestre/3110.php
@@ -1,9 +1,20 @@
 <?php
-
-
-
-
-
-
-
+  //declaração 
+  $matriz = readline();
+  $valor = array();
+  
+  for($i = 0; $i < 5; $i++){
+  for($j = 0; $j < 5; $j++){
+  $valor[$i][$j] = readline();
+  
+} 
+//saída de dados 
+  }
+  for($i = 0; $i < 5; $i++){
+  for($j = 0; $j < 5; $j++){
+  if($valor[$i][$j] >= $matriz){
+  print("Valor Encontrado: ".$valor[$i][$j]." Linha: ".$i." Coluna: ".$j."\n");
+      }
+    } 
+  }
 ?>
