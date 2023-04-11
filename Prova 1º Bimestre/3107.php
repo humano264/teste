32fe0960517aa9cdc26333ce18dd6fd42513add0
MAUMOREIRA <mauriciomoreirajr2006@gmail.com>
2023-04-11 11:58:56 -0300

    mds está certo que glória

--- Prova 1º Bimestre/3107.php
+++ Prova 1º Bimestre/3107.php
@@ -1,16 +1,18 @@
 <?php
+    $velocidade = readline();
+    $distancia = readline();
+    $consumo = readline();
 
-// lê as entradas do usuário
-$velocidadeMedia = floatval(fgets(STDIN));
-$distanciaViagem = floatval(fgets(STDIN));
-$consumoMedio = floatval(fgets(STDIN));
+    $velocidade = number_format($velocidade, 2, ".", "");
+    $distancia = number_format($distancia, 2, ".", "");
+    $consumo = number_format($consumo, 2, ".", "");
 
-// calcula o tempo necessário em horas para completar a viagem
-$tempoViagem = $distanciaViagem / $velocidadeMedia;
+    $horas = $distancia/$velocidade;
+    $combustivel = $distancia/$consumo;
+    $horas = number_format($horas, 2, ".", "");
+    $combustivel = number_format($combustivel, 2, ".", "");
 
-// calcula o total de combustível gasto
-$combustivelGasto = $distanciaViagem / $consumoMedio;
-
-// imprime os resultados
-printf("%.2f\n", $tempoViagem);
-printf("%.2f\n", $combustivelGasto);
+    echo "Total horas: $horas\n";
+    echo "Total combustivel: $combustivel\n";
+    
+?>
